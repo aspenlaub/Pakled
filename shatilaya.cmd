1da95c2109999f6a02163b306030d72ba3e8a18b
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-09 19:21:32 +0100

    Revert "Create shatilaya.cmd"
    This reverts commit e0e976e670357b9a381b90f45800b3d9c0889d5c.

--- shatilaya.cmd
+++
@@ -1,3 +0,0 @@
-cd C:\Users\Wolfgang\GitHub\ShatilayaBin\Release
-C:\Users\Wolfgang\GitHub\ShatilayaBin\Release\Aspenlaub.Net.GitHub.CSharp.Shatilaya.exe --repository C:\Users\Wolfgang\GitHub\Pakled
-pause
