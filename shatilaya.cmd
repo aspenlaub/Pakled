e0e976e670357b9a381b90f45800b3d9c0889d5c
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-02-09 19:20:05 +0100

    Create shatilaya.cmd

---
+++ shatilaya.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\Wolfgang\GitHub\ShatilayaBin\Release
+C:\Users\Wolfgang\GitHub\ShatilayaBin\Release\Aspenlaub.Net.GitHub.CSharp.Shatilaya.exe --repository C:\Users\Wolfgang\GitHub\Pakled
+pause
